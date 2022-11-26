f73e01baa6f2cdc5f2c654d4b0477d11a24990b9
jimirwinjr <snymax@gmail.com>
2022-11-26 10:16:46 -0500

    Added a minimal solve method. This is a theoretical application the runtime and any legitimate use case would be astronomic

--- Sources/SwiftlyDLX/SDLX.swift
+++ Sources/SwiftlyDLX/SDLX.swift
@@ -28,7 +28,7 @@ public struct SDLX {
         for r in grid {
             var rw = Set<Int>()
             for (j, c) in r.enumerated() where c {
-                rw.insert(j)
+                rw += j
             }
             rs.append(rw)
         }
@@ -59,22 +59,27 @@ public struct SDLX {
     
     //MARK: - Solving Methods
     
-    /**
-     Attempt to solve and return first solution found
-     */
-    public mutating func solve() -> Set<Int>? {
-        return generate{ solution in
-            return solution
-        }
-    }
     
-    public mutating func solve(_ rows: Set<Int>) -> Set<Int>? {
+    /**
+     Solve the grid first covering the provided rows and attempt to solve it exactly n number of times
+     
+     This is a general use method of implementing the generate method however generate can be used in other methods.
+     */
+    public mutating func solve(_ rows: Set<Int> = [], _ times: Int) -> Set<Int>? {
         guard let cache = grid.cover(rows) else {
-            print("This combination of values causes an error")
+            print("This is an unsolvable set of rows")
             return nil
         }
-        defer{grid.uncover(cache)}
-        return generate{$0}
+        history += rows
+        defer{
+            grid.uncover(cache)
+            history -= rows
+        }
+        var count = 0
+        return generate{
+            count += 1
+            return count == times ? $0:nil
+        }
     }
     
     @discardableResult
@@ -84,15 +89,53 @@ public struct SDLX {
         }
         for r in column {
             guard let cache = grid.cover(r) else {continue}
-            history.insert(r)
+            history += r
             defer{
-                history.remove(r)
+                history -= r
                 grid.uncover(cache)
             }
             guard let solution = generate(each: each) else {continue}
             return solution
         }
         return nil
+    }
+    
+    /**
+     attempt to find the smallest version of the grid which only has one result. This will have a long runtime and at this time cannot be canceled. The minSize will be a method to for the function to return early if it finds a solution that is  less then or equal to the minimium Size
+     */
+    public mutating func minimal(_ rows: Set<Int>,_ minSize: Int = 0) -> Set<Int>? {
+        guard let cache = grid.cover(rows) else {
+            print("The provided set of rows has no possible solution")
+            return nil
+        }
+        history += rows
+        defer{
+            grid.uncover(cache)
+            history -= rows
+        }
+        
+        return minimalSolve(minSize)
+    }
+    
+    /** WARNING: - The runtime of this method can be extensive and will be blocking. at this time its not cancelable. Later this will be changed*/
+    private mutating func minimalSolve(_ minSize: Int) -> Set<Int>? {
+        guard let column = pickColumn() else { //If no columns left the puzzle is solved so there must only be one solution at this point
+            return history
+        }
+        var solution: Set<Int>? = nil
+        for r in column {
+            guard let cache = grid.cover(r) else {continue} //This row cannot be solved
+            history += r
+            defer{
+                grid.uncover(cache)
+                history -= r
+            }
+            guard let sol = minimalSolve(minSize) else {continue} //solution was found
+            guard solution == nil else {return nil} //previous solution does not exist
+            guard history.count > minSize else {return history} //current solution is greater then the minSize parameter
+            solution = history //Using history as the returned value will be the second to last.
+        }
+        return history
     }
 }
 
